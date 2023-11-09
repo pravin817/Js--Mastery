ce53e0a8814ebe45087276e42f23812a7fc20735
Pravin Mhaske <pravinmhaske817@gmail.com>
2023-11-09 09:06:51 +0530

    Data types added successfully.

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";   // This is a directive that indicates that the code should be executed in "strict mode".
+
+// alert("Hello World");  // we are using node js so alert is not defined
+
+console.log(3 + 3);
+console.log("Prain");
+
+let age = 21
+let name = "Pravin"
+
+
+// number =>  2 to power 53
+// bigInt 
+// string => ""
+// Boolean => true/false 
+// null => standalone value 
+// undefined =>
+// Symbole => used in the react components for the uniqueness
+
+
+// Objects
+
+console.table([typeof undefined, typeof null, typeof age, typeof name]);
